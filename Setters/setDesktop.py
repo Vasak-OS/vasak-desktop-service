0b0c65d5b5bbd6ab01d7b4042dacfea557608946
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-01-06 17:18:32 -0300

    Add Desktop Service Script

---
+++ Setters/setDesktop.py
@@ -0,0 +1,13 @@
+import gi
+gi.require_version('Wnck', '3.0')
+from gi.repository import Wnck
+
+screen = Wnck.Screen.get_default()
+screen.force_update()
+temp_windows = screen.get_windows()
+
+for win in temp_windows:
+  if (win.get_class_instance_name() == 'lynx-desktop'):
+    win.set_window_type(1)
+    win.pin()
+    win.make_below()
