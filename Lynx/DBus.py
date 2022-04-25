a48ac7debf7aabfcf2c0d94d40eea468af242349
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-25 15:41:22 -0300

    Fix to new methods

--- Lynx/DBus.py
+++
@@ -1,6 +0,0 @@
-import dbus
-
-def client(busName):
-    bus = dbus.SessionBus()
-    service = bus.get_object(busName, f"/{busName.replace('.', '/')}")
-    return dbus.Interface(service, dbus_interface=busName)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from Lynx.Master import LynxMaster
-from Lynx.DBus import client
 from Lynx.Notifications import Notifications
 from gi.repository import GLib
 from dbus.mainloop.glib import DBusGMainLoop
@@ -9,10 +8,8 @@ if __name__ == "__main__":
     DBusGMainLoop(set_as_default=True)
     loop = GLib.MainLoop()
 
-    dockDBus = client('ar.net.lynx.os.dock')
-
     lynxNotify = Notifications()
-    lScreen = LynxMaster(dockDBus, lynxNotify)
+    lScreen = LynxMaster(lynxNotify)
 
     loop.run()
 
