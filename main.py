9a85f02d811de37b69a01d4879e4add4dd48ac04
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-26 13:01:30 -0300

    Update main import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Vasak.Master import VasakMaster
-from Vasak.Notifications import Notifications
+from vasak.master import VasakMaster
+from vasak.notifications import Notifications
 from gi.repository import GLib
 from dbus.mainloop.glib import DBusGMainLoop
 
